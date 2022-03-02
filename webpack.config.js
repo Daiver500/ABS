a66045b621bd1557e6d1d26c5ea4fc14b1a2caa6
Aleksei <60549770+Daiver500@users.noreply.github.com>
2022-03-02 15:27:10 +0300

    Webpack prep

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 const path = require('path');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: ["./source/js/app.js"],
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname),
+    path: __dirname + '/source/js',
   },
   mode: 'none',
+  watch: true,
   module: {
     rules: [
       {
         test: /\.(scss)$/,
-        use: [ MiniCssExtractPlugin.loader,'css-loader', {loader: "sass-loader"},
-          options: {
-          publicPath: "./source/css",
-        }],
+
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          "css-loader",
+          "sass-loader",
+        ],
       },
       { test: /\.(js)$/,
         exclude: /(node_modules|bower_components)/,
@@ -29,10 +31,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'style.min.css',
     }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      path:  __dirname + '/js'
-    })
   ],
   optimization: {
     minimizer: [
